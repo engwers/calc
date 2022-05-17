f048bdbf8a58c6aad964cf1285d685d8e404f33f
engwers <morita_1247@yahoo.co.jp>
2022-05-17 18:11:35 +0900

    change indent

--- myfile/myfile.cpp
+++ myfile/myfile.cpp
@@ -5,79 +5,79 @@
 #include "myfile.hpp"
 
 OutMyFile::OutMyFile(const int n, const std::filesystem::path &path) : N(n), file_path(path) {
-	return;
+  return;
 }
 
 void OutMyFile::out(const double * const val) const {
 
-	std::ofstream outfile(file_path);
+  std::ofstream outfile(file_path);
 
-	for (int i = 0; i < N; ++i) {
-		outfile << val[i] << std::endl;
-	}
+  for (int i = 0; i < N; ++i) {
+    outfile << val[i] << std::endl;
+  }
 
-	return;
+  return;
 }
 
 void OutMyFile::out(const double * const xval, const double * const yval) const {
 
-	std::ofstream outfile(file_path);
+  std::ofstream outfile(file_path);
 
-	for (int i = 0; i < N; ++i) {
-		outfile << xval[i] << " " << std::endl;
-	}
+  for (int i = 0; i < N; ++i) {
+    outfile << xval[i] << " " << std::endl;
+  }
 
-	return;
+  return;
 }
 
 InMyFile::InMyFile(const std::filesystem::path &path) :file_path(path), N(getNum()) {
 
-	std::ifstream infile(file_path);
+  std::ifstream infile(file_path);
 	 
-	if (!infile) {
-		std::cout << "cannot open" << std::endl;
-		return;
-	}
+  if (!infile) {
+	  std::cout << "cannot open" << std::endl;
+  	return;
+  }
 
-  arr = new double[N];
-	for (int i = 0; i < N; ++i) {
-		infile >> arr[i];
-	}
-	infile.close();
+	arr = new double[N];
+  for (int i = 0; i < N; ++i) {
+    infile >> arr[i];
+  }
+  infile.close();
 
-	return;
+  return;
 }
 
 InMyFile::~InMyFile() {
-	if (arr != nullptr) {
-		delete[] arr;
-	}
-	return;
+  if (arr != nullptr) {
+    delete[] arr;
+  }
+  return;
 }
 
 inline bool InMyFile::check_index(const int idx) const {
-	if ((idx < 0) || (N <= idx)) return false;
-	return true;
+  if ((idx < 0) || (N <= idx)) return false;
+  return true;
 }
 
 int InMyFile::getNum() const{
 
-	std::ifstream infile(file_path);
+  std::ifstream infile(file_path);
 
-	int count = 0;
-	std::string line; 
+  int count = 0;
+  std::string line; 
 
-	while (std::getline(infile, line)) ++count;
+  while (std::getline(infile, line)) ++count;
 
-	infile.close();
+  infile.close();
 
-	return count;
+  return count;
 }
 
 double InMyFile::operator() (const int idx) const {
-	if(!check_index(idx)) {
-		std::cout << "error: out of index" << std::endl;
-		return 0;
-	}
-	return arr[idx];
+  if(!check_index(idx)) {
+    std::cout << "error: out of index" << std::endl;
+    return 0;
+  }
+  return arr[idx];
 }
